687b6d9e0dccf00ffda82b075ddc0f06a3380e8e
Empix <jotinhave@gmail.com>
2020-12-20 05:52:49 -0400

    Feature: Delete news card and image

--- src/controllers/NewsCardController.js
+++ src/controllers/NewsCardController.js
@@ -1,3 +1,6 @@
+const fs = require('fs/promises');
+const path = require('path');
+
 const connection = require('../database/connection');
 
 module.exports = {
@@ -27,11 +30,20 @@ module.exports = {
   async delete(req, res) {
     const { id } = req.params;
 
+    const { image_url } = await connection('news_cards')
+      .where('id', id)
+      .select('image_url')
+      .first();
+
+    const imageName = image_url.split('images/')[1];
+
+    await fs.unlink(path.resolve(__dirname, '..', '..', 'uploads', imageName));
+
     await connection('news_cards')
       .where('id', id)
       .delete()
       .then((result) => {
-        return res.json({ success: true, result });
+        return res.json({ success: true });
       });
   },
 };
