2bd896dbb27d512aafde1f256b5af6ab7121c56a
Empix <jotinhave@gmail.com>
2020-12-23 16:08:04 -0400

    Add cors

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const cors = require('cors');
 
 const routes = require('./routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
