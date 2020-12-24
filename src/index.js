b5e4cff5a6d5285d5d5d0396fb3b5398854f6383
Empix <jotinhave@gmail.com>
2020-12-24 06:42:48 -0400

    Create error handler

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const cors = require('cors');
+const multer = require('multer');
 
 const routes = require('./routes');
 
@@ -15,5 +16,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use('/images', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
+
+app.use((error, req, res, next) => {
+  if (error instanceof multer.MulterError) {
+    error.status = 400;
+  }
+
+  return res.status(error.status || 500).json({ error: error.message });
+});
 
 app.listen(process.env.PORT || 3333);
