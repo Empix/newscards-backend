bbec4752e9dc868efc60bb5ad66a1e54517b3d08
Empix <jotinhave@gmail.com>
2021-02-16 01:24:11 -0400

    Add findIndex and Update; Fix some things

--- src/controllers/NewsCardController.js
+++ src/controllers/NewsCardController.js
@@ -4,8 +4,21 @@ const path = require('path');
 const connection = require('../database/connection');
 
 module.exports = {
+  async findIndex(req, res) {
+    const { id } = req.params;
+
+    const newscard = await connection('news_cards')
+      .select('*')
+      .where({ id })
+      .first();
+
+    return res.json(newscard);
+  },
+
   async index(req, res) {
-    const newscards = await connection('news_cards').select('*');
+    const newscards = await connection('news_cards')
+      .select('*')
+      .orderBy('id', 'desc');
 
     return res.json(newscards);
   },
@@ -14,7 +27,7 @@ module.exports = {
     const { title, author, url, description } = req.body;
     const { file } = req;
 
-    if (!title || !url || !description || !file) {
+    if (!title || !author || !url || !description || !file) {
       const error = new Error('Fields incorrectly filled');
       error.status = 400;
 
@@ -29,6 +42,48 @@ module.exports = {
         description,
         image_url: `${process.env.APP_URL}/images/${file.filename}`,
       })
+      .then((result) => {
+        return res.json({ success: true });
+      });
+  },
+
+  async update(req, res) {
+    const { title, author, url, description } = req.body;
+    const { file } = req;
+    const { id } = req.params;
+
+    if (!title || !author || !url || !description) {
+      const error = new Error('Fields incorrectly filled');
+      error.status = 400;
+
+      return next(error);
+    }
+
+    const data = {
+      title,
+      author,
+      url,
+      description,
+    };
+
+    if (file) {
+      const { image_url } = await connection('news_cards')
+        .where({ id })
+        .select('image_url')
+        .first();
+
+      const imageName = image_url.split('images/')[1];
+
+      await fs.unlink(
+        path.resolve(__dirname, '..', '..', 'uploads', imageName)
+      );
+
+      data.image_url = `${process.env.APP_URL}/images/${file.filename}`;
+    }
+
+    await connection('news_cards')
+      .update(data)
+      .where({ id })
       .then((result) => {
         return res.json({ success: true });
       });
